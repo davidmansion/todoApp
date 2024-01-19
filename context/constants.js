2b59e000c8607792509d78e3bc206599799e4f4b
papaexchange <info@papaexchange.com>
2024-01-19 07:36:29 +0000

    second commit

---
+++ context/constants.js
@@ -0,0 +1,6 @@
+//0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
+
+import ToDoList from "./ToDoList.json";
+
+export const toDoListAdress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+export const ToDoListAddressABI = ToDoList.abi;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,14 @@
-const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
-export default MyApp
+import "../styles/globals.css";
+
+//INTERNAL IMPORT
+import { VotingProvider } from "../context/ToDoList";
+
+const MyApp = ({ Component, pageProps }) => (
+  <VotingProvider>
+    <div>
+      <Component {...pageProps} />
+    </div>
+  </VotingProvider>
+);
+
+export default MyApp;
